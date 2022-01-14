5364772e0575aa4ec408a05b02b6be0c188c9cd7
zoofboof <91989570+zoofboof@users.noreply.github.com>
2022-01-14 15:56:37 -0500

    Add files via upload

---
+++ RPG Project V1.2/mainV1.py
@@ -0,0 +1,122 @@
+import pygame, sys
+from buttonM import Button
+import RPG_Project_V1_2_1 as RPG
+
+pygame.init()
+
+#Create the screen and set the window caption
+
+SCREEN = pygame.display.set_mode((800, 550))
+pygame.display.set_caption("Yet to be named RPG")
+
+#Set main menu background image
+
+BG = pygame.image.load("Menu_assets/Background1.png")
+
+#Change pygame icon
+
+WindowIcon = pygame.image.load('RPG_Project_items/img/Icons/potion.png')
+pygame.display.set_icon(WindowIcon)
+
+
+def menu():
+    main_menu()
+
+if __name__ == "__menu__":
+    menu()
+
+def get_font(size): # Returns Press-Start-2P in the desired size
+    return pygame.font.Font("Menu_assets/runescape_uf.ttf", size)
+
+def play():
+    while True:
+        PLAY_MOUSE_POS = pygame.mouse.get_pos()
+
+        SCREEN.fill("black")
+
+        RPG.main()
+
+        PLAY_TEXT = get_font(45).render("This is the PLAY screen.", True, "White")
+        PLAY_RECT = PLAY_TEXT.get_rect(center=(640, 260))
+        SCREEN.blit(PLAY_TEXT, PLAY_RECT)
+
+        PLAY_BACK = Button(image=None, pos=(640, 460), 
+                            text_input="BACK", font=get_font(75), base_color="White", hovering_color="Green")
+
+        PLAY_BACK.changeColor(PLAY_MOUSE_POS)
+        PLAY_BACK.update(SCREEN)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if PLAY_BACK.checkForInput(PLAY_MOUSE_POS):
+                    main_menu()
+
+        pygame.display.update()
+    
+def options():
+    while True:
+        OPTIONS_MOUSE_POS = pygame.mouse.get_pos()
+
+        SCREEN.fill("white")
+
+        OPTIONS_TEXT = get_font(45).render("The Lush Life is Giving Me a Rush!", True, "Black")
+        OPTIONS_RECT = OPTIONS_TEXT.get_rect(center=(400, 225))
+        SCREEN.blit(OPTIONS_TEXT, OPTIONS_RECT)
+
+        OPTIONS_BACK = Button(image=None, pos=(640, 460), 
+                            text_input="BACK", font=get_font(75), base_color="Black", hovering_color="Green")
+
+        OPTIONS_BACK.changeColor(OPTIONS_MOUSE_POS)
+        OPTIONS_BACK.update(SCREEN)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if OPTIONS_BACK.checkForInput(OPTIONS_MOUSE_POS):
+                    main_menu()
+
+        pygame.display.update()
+
+def main_menu():
+    while True:
+        SCREEN.blit(BG, (0, 0))
+
+        MENU_MOUSE_POS = pygame.mouse.get_pos()
+
+        MENU_TEXT = get_font(60).render("MAIN MENU", True, "#b68f40")
+        MENU_RECT = MENU_TEXT.get_rect(center=(400, 100))
+
+        PLAY_BUTTON = Button(image=pygame.image.load("RPG_Project_items/img/Icons/Button_background.png"), pos=(400, 200), 
+                            text_input="PLAY", font=get_font(35), base_color="#d7fcd4", hovering_color="White")
+        OPTIONS_BUTTON = Button(image=pygame.image.load("RPG_Project_items/img/Icons/Button_background.png"), pos=(400, 325), 
+                            text_input="OPTIONS", font=get_font(35), base_color="#d7fcd4", hovering_color="White")
+        QUIT_BUTTON = Button(image=pygame.image.load("RPG_Project_items/img/Icons/Button_background.png"), pos=(400, 450), 
+                            text_input="QUIT", font=get_font(35), base_color="#d7fcd4", hovering_color="White")
+
+        SCREEN.blit(MENU_TEXT, MENU_RECT)
+
+        for button in [PLAY_BUTTON, OPTIONS_BUTTON, QUIT_BUTTON]:
+            button.changeColor(MENU_MOUSE_POS)
+            button.update(SCREEN)
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if PLAY_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    play()
+                if OPTIONS_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    options()
+                if QUIT_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    pygame.quit()
+                    sys.exit()
+
+        pygame.display.update()
+
+menu()
